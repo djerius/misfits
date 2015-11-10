17b85b6ffe32d410db869baaaffaaf7084f8e380
Diab Jerius <djerius@cfa.harvard.edu>
2015-11-10 15:12:17 -0500

    merge temptable.cc into table.cc

--- tests/table.cc
+++ tests/table.cc
@@ -30,7 +30,7 @@ namespace Entity = misFITS::Entity;
 namespace Mode = misFITS::Mode;
 
 
-class TableTest : public ::testing::Test {
+class ExistingTable : public ::testing::Test {
 
 protected :
 
@@ -43,7 +43,7 @@ protected :
 };
 
 
-TEST_F( TableTest, MetaData ) {
+TEST_F( ExistingTable, MetaData ) {
 
     EXPECT_STREQ( TEST_FITS_QFILENAME, file->file.c_str());
 
@@ -56,9 +56,44 @@ TEST_F( TableTest, MetaData ) {
     ASSERT_TRUE( table.exists_column( "Ecol" ) );
     ASSERT_TRUE( table.exists_column( "Dcol" ) );
 
-    ASSERT_EQ( 1, table.column("Icol" ).colnum );
+    ASSERT_EQ( 1, table.column( "Icol" ).colnum );
     ASSERT_EQ( 2, table.column( "Jcol" ).colnum );
     ASSERT_EQ( 3, table.column( "Ecol" ).colnum );
     ASSERT_EQ( 4, table.column( "Dcol" ).colnum );
+}
+
+TEST( TableTest, CreateTable ) {
+
+    misFITS::Table table( "MYEXTENT" );
+
+    EXPECT_EQ( "MYEXTENT",  table.extname );
+
+    EXPECT_EQ( 0, table.num_columns() );
+
+    table.add( "col2", misFITS::CT_DOUBLE, 1 );
+    table.add( "col1", misFITS::CT_DOUBLE, 1, 1 );
+    table.add( "col3", misFITS::CT_DOUBLE, 1, 0 );
+
+    EXPECT_EQ( 3, table.num_columns() );
+    EXPECT_EQ( "col1", table.column(1).ttype );
+    EXPECT_EQ( "col2", table.column(2).ttype );
+    EXPECT_EQ( "col3", table.column(3).ttype );
+
+    misFITS::FilePtr file( misFITS::open<Entity::Memory>() );
+    table.copy( file );
+
+
+    misFITS::Table table2( file );
+
+    EXPECT_EQ( 3, table2.num_columns() );
+    EXPECT_EQ( "col1", table2.column(1).ttype );
+    EXPECT_EQ( "col2", table2.column(2).ttype );
+    EXPECT_EQ( "col3", table2.column(3).ttype );
+
+    table2.add( "col4", misFITS::CT_DOUBLE, 1, 0 );
+
+    EXPECT_EQ( 3, table.num_columns() );
+    EXPECT_EQ( 4, table2.num_columns() );
+
 }
 
