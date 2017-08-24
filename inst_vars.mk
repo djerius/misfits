c30945618ce6a05526ef0a6968157465f79f969b
Diab Jerius <djerius@cfa.harvard.edu>
2017-08-24 17:03:52 -0400

    update snippets

--- inst_vars.mk
+++ inst_vars.mk
@@ -1,8 +1,14 @@
-#============================================================================
-# inst_vars.mk
+##============================================================================
+## inst_vars.mk
 
 INST_VARS_MK =
 
-# our version of inst-vars.am
+## our version of inst-vars.am
 
-strip_dir = `echo $$p | sed -e 's|^.*/||'`;
+strip_dir	= `echo $$p | sed -e 's|^.*/||'`;
+strip_sfx	= `echo $$p | sed -e 's|[.][^.]*$$||'`
+mst__dirname    = `expr "$$p" : "\(.*\)/[^/]*"`
+mst__basename   = `expr "$$p" : ".*/\([^/]*\)$$" || echo "$$p"`
+mst__strip_sfx  = `expr "$$p" : "\(.*\)\.[^./]*"`
+mst__strip_pfx  = `expr "$$p" : "^$$D\(.*\)"`
+mst__dirstamp   = $(am_leading_dot)/dirstamp
